67fff6a879a6425a8b34eea32444c406320d1a24
Rodrigo Tognin <rotog@outlook.com>
2021-09-04 16:54:02 -0300

    Testes de arquivos existentes ou não no projeto

--- tests/ArquivoTest.php
+++ tests/ArquivoTest.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Testes específicos para a estrutura do projeto:
+ * - Se os arquivos necessários existem
+ * - Se alguns aquivos que não devem ir para produção estão no projeto
+ */
+
 namespace Tests;
 
 use PHPUnit\Framework\TestCase;
@@ -7,9 +13,9 @@ use PHPUnit\Framework\TestCase;
 class ArquivoTest extends TestCase
 {
     /**
-     * @dataProvider dpTestInsert
+     * @dataProvider dpTestExiste
      */
-    public function testInsert(string $arquivo)
+    public function testExiste(string $arquivo)
     {
         $this->assertTrue(file_exists($arquivo), 'Faltando arquivo ' . $arquivo . ' no projeto!');
     }
@@ -18,7 +24,7 @@ class ArquivoTest extends TestCase
      * Arquivos necessários para o projeto rodar (por exemplo...)
      * Se estiver faltando algum arquivo, o teste falhará
      */
-    public function dpTestInsert()
+    public function dpTestExiste()
     {
         return array(
             array('.gitignore'), 
@@ -28,4 +34,25 @@ class ArquivoTest extends TestCase
             //array('existe.nao') // esse não existe... Faz o teste falhar
         );
     }
+
+    /**
+     * @dataProvider dpTestNaoExiste
+     */
+    public function testNaoExiste(string $arquivo)
+    {
+        $this->expectError(); // "expectException" marcada como obsoleta
+        include $arquivo;
+    }
+
+    /**
+     * Provedor dos arquivos que não devem existir no projeto em Produção
+     */
+    public function dpTestNaoExiste()
+    {
+        return array(
+            array('config_dev.php'),
+            array('App/Config/database_dev.php')
+        );
+    }
+
 }
